fea8554d9555a299ee81e49b23445c12e74c8df9
mouli3088 <37960178+mouli3088@users.noreply.github.com>
2018-04-10 07:13:34 +0530

    Update ReaderWriterProblem.c

--- ReaderWriterProblem.c
+++ ReaderWriterProblem.c
@@ -4,12 +4,13 @@
 #include<time.h>
 #include<stdlib.h>
 pthread_mutex_t writeraccess,readeraccess;
-int value=90;
+int value=90,rc,wc;
 int readcount=0,SleepTime=5;
 void *reader(void *arg1)
 {
 int temp=*((int *)arg1)+1;
-printf("\nReader%d is trying to enter",temp);
+rc++; 
+printf("\nReader%d is trying to enter",rc);
 sleep(rand()%SleepTime);
 pthread_mutex_lock(&readeraccess);
 readcount++;
@@ -17,37 +18,36 @@ if(readcount==1)
 {
 pthread_mutex_lock(&writeraccess);
 }
-printf("\nReader%d is Inside",temp);
+printf("\nReader%d is Inside",readcount);
 printf("\nNumber of readers inside are %d",readcount);
-printf("\nValue read by reader%d is :%d",temp,value);
+printf("\nValue read by reader%d is :%d",rc,value);
 pthread_mutex_unlock(&readeraccess);
 sleep(rand()%SleepTime);
 pthread_mutex_lock(&readeraccess);
 readcount--;
-//add
+
 if(readcount==0)
 {
 pthread_mutex_unlock(&writeraccess);
 }
 pthread_mutex_unlock(&readeraccess);
-printf("\nReader Left is %d",temp);
+rc--;
+printf("\nReader Left is %d",rc+1);
 sleep(rand()%SleepTime);
 }
 void *writer(void *arg2)
 {
 int temp2=*((int *)arg2)+1;
-int writer_count;
-printf("\nWriter %d wants to enter",temp2);
-sleep(rand()%SleepTime);
+wc++;
+printf("\nWriter %d wants to enter",wc);
 pthread_mutex_lock(&writeraccess);
-printf("\nWriter entered is %d ",temp2);
+printf("\nWriter entered is %d ",wc);
 value=value+10;
-//
-printf("\nValue after written by writer%d is %d",temp2,value);
+printf("\nValue after written by writer%d is %d",wc,value);
 sleep(rand()%SleepTime);
 pthread_mutex_unlock(&writeraccess);
-writer_count--;
-printf("\nWriter left is %d",temp2);
+wc--;
+printf("\nWriter left is %d",wc+1);
 sleep(rand()%SleepTime);
 }
 int main()
